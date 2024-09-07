edbff70da4bf6c4b521835050ae4deecb0ea3c44
unknown <thomax4blues@gmail.com>
2024-09-06 17:43:44 -0700

    new updates

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dell\Desktop\MultiDynamicServices\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\MY PROJECT\Desktop\MultiDynamicServices\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
